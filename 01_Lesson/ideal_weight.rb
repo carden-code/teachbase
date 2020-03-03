2ba7bb407caf464d5a40ed12ae388232a8f389d5
carden <carden.ruby@gmail.com>
2020-03-03 16:40:01 +0300

    first version

---
+++ 01_Lesson/ideal_weight.rb
@@ -0,0 +1,13 @@
+# The program asks the user for a name and height and displays the ideal weight
+# according to the formula, after which it displays the result to the user
+# on the screen with a call by name.
+puts 'Please write your name:'
+name = gets.chomp
+puts 'Please indicate your height:'
+height = gets.chomp.to_i
+ideal_weight = (height - 110) * 1.15
+if ideal_weight.negative?
+  puts 'Your weight is already optimal'
+else
+  puts "#{name}, your ideal weight - #{ideal_weight}"
+end
