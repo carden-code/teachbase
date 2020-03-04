5cec35f8ff5d5011d0f2dfb3f790f47964ca3617
carden <carden.ruby@gmail.com>
2020-03-04 15:22:51 +0300

    bugs fixed

--- 01_Lesson/quadratic_equation.rb
+++ 01_Lesson/quadratic_equation.rb
@@ -3,22 +3,21 @@
 # the values ​​of the discriminant and roots on the screen.
 puts 'Please enter three coefficients.'
 puts 'First coefficient:'
-a = gets.chomp.to_i
+a = gets.chomp.to_f
 puts 'Second coefficient:'
-b = gets.chomp.to_i
+b = gets.chomp.to_f
 puts 'Third coefficient:'
-c = gets.chomp.to_i
+c = gets.chomp.to_f
 dis = b**2 - (4 * a * c)
-first = - b + Math.sqrt(dis) / 2 * a
-second = - b - Math.sqrt(dis) / 2 * a
 if dis.negative?
   puts "Discriminant: #{dis}"
   puts 'No roots.'
 elsif dis.positive?
+  sqr = Math.sqrt(dis)
   puts "Diskriminant: #{dis}"
-  puts "First root of the equation: #{first}"
-  puts "Second root of the equation: #{second}"
+  puts "First root of the equation: #{- b + sqr / 2 * a}"
+  puts "Second root of the equation: #{- b - sqr / 2 * a}"
 elsif dis.zero?
   puts "Diskriminant: #{dis}"
-  puts "Single root of the equation: #{first}"
+  puts "Single root of the equation: #{- b / 2 * a}"
 end
