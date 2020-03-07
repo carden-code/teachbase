f5f4620c6d01bf974e99a90e0a4b03984ddbab35
carden <carden.ruby@gmail.com>
2020-03-07 13:28:37 +0300

    .take().sum

--- 02_Lesson/date_serial_number.rb
+++ 02_Lesson/date_serial_number.rb
@@ -6,16 +6,8 @@ puts 'Month:'
 month = gets.chomp.to_i
 puts 'Year:'
 year = gets.chomp.to_i
-hash = { 1 => 31, 2 => 28, 3 => 31, 4 => 30, 5 => 31, 6 => 30,
-         7 => 31, 8 => 31, 9 => 30, 10 => 31, 11 => 30, 12 => 31 }
-hash[2] = 29 if ((year % 4).zero? && year % 100 != 0) || (year % 400).zero?
-sum = 0
-if month > 0
-  hash.each do |kay, value|
-    sum += value
-    break if kay == month - 1
-  end
-  date + sum
-end
+arr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+arr[1] = 29 if ((year % 4).zero? && year % 100 != 0) || (year % 400).zero?
+sum = arr.take(month - 1).sum
 puts "Date entered: #{date}.#{month}.#{year}"
 puts "Date serial number: #{date + sum}"
