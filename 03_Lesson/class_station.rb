407b05a0e9957c19e8f87dcd624a178b9cdc8f6a
carden <carden.ruby@gmail.com>
2020-03-10 22:29:23 +0300

    description added

--- 03_Lesson/class_station.rb
+++ 03_Lesson/class_station.rb
@@ -1,3 +1,11 @@
+# Класс Station (Станция):
+#  Имеет название, которое указывается при ее создании.
+#  Может принимать поезда (по одному за раз).
+#  Может возвращать список всех поездов на станции, находящиеся в текущий момент
+#  Может возвращать список поездов на станции по типу (см. ниже):
+# кол-во грузовых, пассажирских.
+#  Может отправлять поезда (по одному за раз, при этом, поезд удаляется из
+# списка поездов, находящихся на станции).
 class Station
   attr_reader :trains
   def initialize(name)
@@ -16,12 +24,12 @@ class Station
     end
   end
 
-  def type
-    puts "freight trains in station - #{@freight.size}"
-    puts "passenger trains in station - #{@passenger.size}"
+  def retern_type
+    @freight.size
+    @passenger.size
   end
 
   def send
     @trains = @trains.size - 1
   end
-end 
+end
