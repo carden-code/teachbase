9cac53c357b9eecc792f625da56ca3bf78246439
carden <carden.ruby@gmail.com>
2020-03-17 23:28:19 +0300

    commet for class

--- 04_Lesson/cargo_wagon.rb
+++ 04_Lesson/cargo_wagon.rb
@@ -1,3 +1,7 @@
+# Класс CargoWagon (Грузовой Вагон):
+# Имеет номер (number - указывается при создании экземпляра)
+# и тип (cargow - грузовой вагон).
+# Может возвращать тип (type).
 class CargoWagon
   attr_reader :type
   def initialize(number, type = 'cargow')
