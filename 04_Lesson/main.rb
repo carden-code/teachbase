38c3ba5151ab98bf9b3296f400ea12fee1270221
carden <carden.ruby@gmail.com>
2020-03-17 23:35:43 +0300

    main

---
+++ 04_Lesson/main.rb
@@ -0,0 +1,7 @@
+require_relative 'train'
+require_relative 'passenger_train'
+require_relative 'passenger_wagon'
+require_relative 'cargo_train'
+require_relative 'cargo_wagon'
+require_relative 'route'
+require_relative 'station'
