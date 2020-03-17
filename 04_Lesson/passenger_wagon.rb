0d6b38f8be8bab1f8dddc610c930eeb2b6542aca
carden <carden.ruby@gmail.com>
2020-03-17 23:27:49 +0300

    comment for class

--- 04_Lesson/passenger_wagon.rb
+++ 04_Lesson/passenger_wagon.rb
@@ -1,3 +1,7 @@
+# Класс PassengerWagon (Пассажирский Вагон):
+# Имеет номер (number - указывается при создании экземпляра)
+# и тип (passw - пасажирский вагон).
+# Может возвращать тип (type).
 class PassengerWagon
   attr_reader :type
   def initialize(number, type = 'passw')
