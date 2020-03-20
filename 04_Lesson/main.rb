f5acf5e2ad04173872066cd2bb33b541c30effa7
carden <carden.ruby@gmail.com>
2020-03-20 21:34:01 +0300

    ...

--- 04_Lesson/main.rb
+++ 04_Lesson/main.rb
@@ -1,3 +1,4 @@
+#
 require_relative 'train'
 require_relative 'route'
 require_relative 'wagon'
@@ -8,10 +9,19 @@ require_relative 'cargo_wagon'
 require_relative 'passenger_train'
 require_relative 'passenger_wagon'
 
+BORDERLINE = '-' * 50
+
+#
 railway = Railway.new
+
 puts "Добро пожаловать в программу 'Железная дорога'"
+
+#
 loop do
-  railway.menu
-  input = gets.chomp
-  break if input == '0'
+  railway.menu_items
+
+  menu_item = gets.chomp
+  break if menu_item == '0'
+
+  railway.selected(menu_item)
 end
