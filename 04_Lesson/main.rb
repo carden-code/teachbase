032cfe623c917684fc9f62649ef88ee408f364ae
carden <carden.ruby@gmail.com>
2020-03-22 21:44:33 +0300

    modification

--- 04_Lesson/main.rb
+++ 04_Lesson/main.rb
@@ -3,21 +3,26 @@ require_relative 'train'
 require_relative 'route'
 require_relative 'wagon'
 require_relative 'station'
-require_relative 'railway'
 require_relative 'cargo_train'
 require_relative 'cargo_wagon'
 require_relative 'passenger_train'
 require_relative 'passenger_wagon'
+require_relative 'railway'
 
 BORDERLINE = '-' * 50
+NEWLINE = "\n" * 2
 
 #
 railway = Railway.new
-
+puts NEWLINE
 puts "Добро пожаловать в программу 'Железная дорога'"
-
+puts NEWLINE
 #
 loop do
+  puts "stations: #{railway.stations}\n\n"
+  puts "trains: #{railway.trains}\n\n"
+  puts "wagons: #{railway.wagons}\n\n"
+  puts NEWLINE
   railway.menu_items
 
   menu_item = gets.chomp
@@ -25,6 +30,3 @@ loop do
 
   railway.selected(menu_item)
 end
-puts railway.stations
-#puts railway.trains
-#puts railway.wagons
