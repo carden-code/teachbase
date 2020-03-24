03ad3aa6a52791a7be982fb44a38d472c0688d82
carden <carden.ruby@gmail.com>
2020-03-25 00:40:39 +0300

    modif

--- 04_Lesson/main.rb
+++ 04_Lesson/main.rb
@@ -3,11 +3,11 @@ require_relative 'train'
 require_relative 'route'
 require_relative 'wagon'
 require_relative 'station'
+require_relative 'railway'
 require_relative 'cargo_train'
 require_relative 'cargo_wagon'
 require_relative 'passenger_train'
 require_relative 'passenger_wagon'
-require_relative 'railway'
 
 BORDERLINE = '-' * 50
 NEWLINE = "\n" * 2
