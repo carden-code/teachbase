387937edfee537de34ec4a2aa15460980a71bbdb
carden <carden.ruby@gmail.com>
2020-03-31 14:37:22 +0300

    comments

--- 05_Lesson/instance_counter.rb
+++ 05_Lesson/instance_counter.rb
@@ -1,25 +1,30 @@
-#
+# модуль InstanceCounter, содержит методы класса и инстанс-методы,
+# которые подключаются автоматически при вызове include в классе:
 module InstanceCounter
   def self.included(base)
     base.extend ClassMethods
     base.send :include, InstanceMethods
   end
 
-  #
+  # Модуль ClassMethods содержит методы класса.
   module ClassMethods
+    # Метод instances возвращает кол-во экземпляров данного класса.
     def instances
       @instances
     end
 
+    # Метод register_count прибавляет к инстанс переменно +1.
     def register_count
       @instances += 1
     end
   end
 
-  #
+  # Модуль InstanceMethods создержит инстанс методы.
   module InstanceMethods
     private
 
+    # Приватный метод register_instance увеличивает счетчик кол-ва экземпляров
+    # класса и который можно вызвать из конструктора.
     def register_instance
       self.class.register_count
     end
