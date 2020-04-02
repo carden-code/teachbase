d7ed69b0b66929d1ddf11dedd27da926dbe56cc4
carden <carden.ruby@gmail.com>
2020-04-02 17:07:19 +0300

    comments

--- 06_Lesson/wagon.rb
+++ 06_Lesson/wagon.rb
@@ -1,16 +1,19 @@
 require_relative 'module_company'
+
 # Класс Wagon (Вагон):
 # Имеет тип (указывается при создании экземпляра)
 # Может возвращать тип
 class Wagon
+  # Подключение модуля Company.
   include Company
-
+  # метод type возвращает type объекта.
   attr_reader :type
   def initialize(type)
     @type = type
     validate!
   end
 
+  # Метод valid? проверяет валидность объекта.
   def valid?
     validate!
     true
@@ -20,6 +23,8 @@ class Wagon
 
   private
 
+  # Приватный метод validate! выбрасывает исключение
+  # если есть несоответствие условию.
   def validate!
     raise 'Ошибка! Тип не указан!' if @type.nil? || @type.empty?
   end
