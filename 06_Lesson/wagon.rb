4852cc549087cbf6a3606a0895222085579fa46e
carden <carden.ruby@gmail.com>
2020-04-02 16:32:30 +0300

    valid?

--- 06_Lesson/wagon.rb
+++ 06_Lesson/wagon.rb
@@ -8,5 +8,17 @@ class Wagon
   attr_reader :type
   def initialize(type)
     @type = type
+    validate!
+  end
+
+  def valid?
+    validate!
+    true
+  rescue StandardError
+    false
+  end
+
+  def validate!
+    raise 'Введен не правильны формат' if type != 'pass' && type != 'cargo'
   end
 end
