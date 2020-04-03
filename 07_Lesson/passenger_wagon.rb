700b0a05e98830696e794d04ea3acb817b1851ca
carden <carden.ruby@gmail.com>
2020-04-03 21:53:19 +0300

    seat

--- 07_Lesson/passenger_wagon.rb
+++ 07_Lesson/passenger_wagon.rb
@@ -4,7 +4,22 @@
 # Имеет тип при создании экземпляра (passw - пасажирский вагон).
 # Может возвращать тип (type).
 class PassengerWagon < Wagon
-  def initialize
+  # Метод seat возвращает колличество свободных мест.
+  attr_reader :seat
+  def initialize(seat)
     super 'pass'
+    @seat = seat
+    @occupied = 0
+  end
+
+  # Метод take_seat "занимает места" в вагоне (по одному за раз).
+  def take_seat
+    @seat -= 1
+    @occupied += 1
+  end
+
+  # Метод number_occupied_seats возвращает кол-во занятых мест в вагоне
+  def number_occupied_seats
+    @occupied
   end
 end
