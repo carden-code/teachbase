140915c68569e17611b902924ff72ce5b08fec4d
carden <carden.ruby@gmail.com>
2020-04-03 22:08:43 +0300

    modification

--- 07_Lesson/cargo_wagon.rb
+++ 07_Lesson/cargo_wagon.rb
@@ -4,7 +4,18 @@
 # Имеет тип при создании экземпляра (cargow - грузовой вагон).
 # Может возвращать тип (type).
 class CargoWagon < Wagon
-  def initialize
+  # Метод volume возвращает оставшийся (доступный) объем.
+  # Метод takes возвращает занятый объем.
+  attr_reader :volume, :takes
+  def initialize(volume)
     super 'cargo'
+    @volume = volume
+    @takes = 0
+  end
+
+  # Метод takes_volume "занимает объем" в вагоне.
+  def takes_volume(volume)
+    @volume -= volume
+    @takes += volume
   end
 end
