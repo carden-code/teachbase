75d0dd4a90897b5e9f40f9ffbd8dbec77c5523a9
carden <carden.ruby@gmail.com>
2020-04-04 18:31:42 +0300

    modification

--- 07_Lesson/cargo_wagon.rb
+++ 07_Lesson/cargo_wagon.rb
@@ -5,17 +5,15 @@
 # Может возвращать тип (type).
 class CargoWagon < Wagon
   # Метод takes возвращает занятый объем.
-  attr_reader :takes
   def initialize(capacity)
     super 'cargo', capacity
     validate!
-    @takes = 0
   end
 
   # Метод takes_volume "занимает объем" в вагоне.
   def takes_volume(capacity)
     return if @capacity < capacity || capacity <= 0
     @capacity -= capacity
-    @takes += capacity
+    @occupied += capacity
   end
 end
