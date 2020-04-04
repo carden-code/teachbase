33e8debbe17524c57c6ea88f9c5b39c1c971309d
carden <carden.ruby@gmail.com>
2020-04-04 18:32:46 +0300

    modification

--- 07_Lesson/wagon.rb
+++ 07_Lesson/wagon.rb
@@ -1,4 +1,5 @@
 require_relative 'module_company'
+require_relative 'instance_counter'
 
 # Класс Wagon (Вагон):
 # Имеет тип (указывается при создании экземпляра)
@@ -8,11 +9,13 @@ class Wagon
   include Company
   # метод type возвращает type объекта.
   # Метод capacity возвращает доступную вместимость.
-  attr_reader :type, :capacity
+  # Метод occupied возвращает кол-во занятого места в вагоне.
+  attr_reader :type, :capacity, :occupied
   def initialize(type, capacity)
     @capacity = capacity.to_i
     @type = type
     validate!
+    @occupied = 0
   end
 
   # Метод valid? проверяет валидность объекта.
