cc077bbab1b64183f5342c129489a8c7e444d613
carden <carden.ruby@gmail.com>
2020-04-09 18:33:27 +0300

    rename module

--- 09_Lesson/module_accessors.rb
+++ 09_Lesson/module_accessors.rb
@@ -1,12 +1,12 @@
 #
-module MyAttrAccessor
+module Accessor
   def self.included(base)
     base.extend ClassMethods
     base.send :include, InstanceMethods
   end
   #
   module ClassMethods
-    def my_attr_accessor_with_history(*names)
+    def attr_accessor_with_history(*names)
       names.each do |name|
         var_name = "@#{name}".to_sym
         define_method(name) { instance_variable_get(var_name) }
