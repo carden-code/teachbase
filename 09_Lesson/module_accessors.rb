c5e3fc45d82746a73f84ec8594d1348b181d0c61
carden <carden.ruby@gmail.com>
2020-04-10 19:10:49 +0300

    modification

--- 09_Lesson/module_accessors.rb
+++ 09_Lesson/module_accessors.rb
@@ -1,41 +1,53 @@
 #
 module Accessor
+  # Метод динамически создает геттеры и сеттеры для любого кол-ва атрибутов,
+  # при этом сеттер сохраняет все значения инстанс-переменной при изменении
+  # этого значения. Также  добавляется инстанс-метод который возвращает
+  # массив всех значений данной переменной.
   def attr_accessor_with_history(*names)
     names.each do |name|
-      # Переменная var_name = имя преданное в блок и становится символом
+      # Переменная var_name = имя преданное в блок и становится символом.
       var_name = "@#{name}".to_sym
-      # Переменная var_history = имя преданное в блок и становится символом
+      # Переменная var_history = имя преданное в блок и становится символом.
       var_history = "@#{name}_history".to_sym
-      # Создаётся метод геттер с именем переданным в блок
+
+      # Создаётся метод геттер с именем переданным в блок.
       define_method(name.to_sym) { instance_variable_get(var_name) }
-      # Создаётся метод геттер с именем переданным в блок +_history
+      # Создаётся метод геттер с именем переданным в блок +_history.
       define_method("#{name}_history".to_sym) { instance_variable_get(var_history) }
-      # Создаётся метод с именем переданным в блок + = и принимает блок значениe
+
+      # Создаётся метод с именем переданным в блок и принимает блок значениe.
       define_method("#{name}=".to_sym) do |value|
-        # Переменная history = значнию интсанс_гет или пустому массиву если значения нет
+        # history = значнию интсанс_гет или пустому массиву если значения нет.
         history = instance_variable_get("@#{name}_history") || []
-        # В эту инстанс переменную "@#{name}_history" сохраняются значения инстанс переменной var_name
+        # В переменную "history" сохраняется значение переменной var_name.
         history << instance_variable_get(var_name)
-        # Этот метод присваивает значение переменной var_name
+        # Этот метод присваивает значение переменной var_name.
         instance_variable_set(var_name, value)
-        # Этот метод присваивает значение переменной var_history
+        # Этот метод присваивает значение переменной var_history.
         instance_variable_set(var_history, history)
       end
     end
   end
 
-  #
-#   def strong_attr_accessor(name, type)
-#     var_name = "@#{name}".to_sym
-#     define_method(name.to_sym) { instance_variable_get(var_name) }
-#     var_name.class == type.class
-#   rescue StandardError => e
-#     puts e.inspect
-#     define_method("#{name}=".to_sym) { |type| instance_variable_set(var_name, type) }
-#   end
-# end
+  # Метод который принимает имя атрибута и его класс. При этом создается геттер
+  # и сеттер для одноименной инстанс-переменной, но сеттер проверяет тип
+  # присваемого значения. Если тип отличается от того,
+  # который указан вторым параметром, то выбрасывается исключение.
+  # Если тип совпадает, то значение присваивается.
+  def strong_attr_accessor(name, type)
+    # Сохраняет в переменную имя атрибута и преобразует в символ.
+    var_name = "@#{name}".to_sym
 
-class X
-  extend Accessor
-  attr_accessor_with_history :a, :b, :c
+    # Создаётся метод геттер.
+    define_method(name.to_sym) { instance_variable_get(var_name) }
+
+    # Создаётся сеттер и присваивает значение если типы совпадают.
+    define_method("#{name}=".to_sym) do |value|
+      # Выбрасит исключение если классы не совпадают.
+      raise TypeError, 'Неправильный тип аргумента!' unless value.class == type
+      # Присваивает значение переданное в блок.
+      instance_variable_set(var_name, value)
+    end
+  end
 end
